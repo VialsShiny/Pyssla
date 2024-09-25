aa23fd085d99da39e2a8ce53664366d2983c92a6
VialsShiny <150128459+VialsShiny@users.noreply.github.com>
2024-09-25 16:11:22 +0200

    Logique du jeu pas finis

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,16 +1,49 @@
 let colorsButtons = document.querySelectorAll('.colors__wrap button');
-const canvaDrawContainer = document.querySelector('.canva-draw__container div.row__wrap');
+const canvaDrawContainer = document.querySelector('.canva-draw__container');
+let changeColor = document.querySelector('.change-color');
+let actualColor = document.querySelector('.actual-color');
 
 colorsButtons.forEach(button => {
-  button.style.backgroundColor = '#' + button.value;
+  button.style.backgroundColor = button.value;
 });
 
-for (let i = 0; i <= 5 * 13; i++) {
-  let div = document.createElement('div');
-  canvaDrawContainer.append(div);
+function CreateCanvaDraw(maxHorizontal, maxVertical) {
+  for (let i = 0; i < maxVertical; i++) {
+    let rowWrap = document.createElement('div');
+    rowWrap.classList.add('row__wrap');
+    canvaDrawContainer.append(rowWrap);
+
+    for (let j = 0; j < maxHorizontal; j++) {
+      let div = document.createElement('div');
+      rowWrap.append(div);
+    }
+  }
 }
 
+CreateCanvaDraw(prompt('Horizontale !'), prompt('Verticale !'));
 
+function chnageColorDraw(color) {
+  this.color = color;
 
+  
+}
+
+let rowWrapDivs = document.querySelectorAll('.row__wrap div');
+
+changeColor.addEventListener('input', () => {
+  actualColor.style.backgroundColor = changeColor.value;
+  actualColor.value = changeColor.value;
+});
+
+rowWrapDivs.forEach(div => {
+  div.addEventListener('click', () => {
+    div.style.backgroundColor = changeColor.value;
+    if (changeColor.value !== "#ffffff") {
+      div.classList.add('drawed')
+    } else {
+      div.classList.remove('drawed')
+    }
+  });
+});
 
 
